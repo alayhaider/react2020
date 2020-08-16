c3ab3f32edf1ff4e108b5bd17d7a3e579be0c9ee
alayhaider <alay.ffp@gmail.com>
2020-08-16 21:53:16 +0500

    Context API

--- ex2_useContext/src/App.js
+++ ex2_useContext/src/App.js
@@ -2,26 +2,26 @@ import React,{useState} from 'react';
 import CounterContext from './CounterContext';
 import Message from './Message';
 
+
+
+
 import './App.css';
 
 
 function App() {
 
-  
 
   
 let[count, setCount] = useState(0);
 
-
-
-
   return (
     <div>
-      
+            
       <CounterContext.Provider value={count}>
      <Message />     
      </CounterContext.Provider>
-         
+    
+
 
      <button onClick=
       {
