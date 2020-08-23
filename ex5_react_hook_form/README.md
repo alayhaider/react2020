2d99d1e95471c8f321de7e948ef5e434dd97bbc3
alayhaider <alay.ffp@gmail.com>
2020-08-23 10:30:23 +0500

    Login API

--- ex5_react_hook_form/README.md
+++ ex5_react_hook_form/README.md
@@ -1,3 +1,5 @@
 # React Hooks Forms
 
 [References](https://github.com/react-hook-form/react-hook-form/tree/master/examples)
+
+[API Example](https://codesandbox.io/s/flamboyant-mirzakhani-yeyzh?file=/src/components/User.js:0-465)
--- ex5_react_hook_form/src/App.js
+++ ex5_react_hook_form/src/App.js
@@ -1,47 +1,65 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { useForm } from "react-hook-form";
-
-
 import './App.css';
 
 
 
 
+
+
+
+
+
+
+
 function App() {
-  
+
+
   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-  const { register, handleSubmit, errors, formState } = useForm();
+  const { register, handleSubmit, errors  } = useForm();
   const onSubmit = async data => {
     await sleep(2000);
-    if (data.username === "bill") {
+    alert(data.pno);
+//    if (data.username === "a") {
+  //http://crewserver1.piac.com.pk/TMS/TMS.asmx/Login?pno=60987&password=piaffp
       alert(JSON.stringify(data));
-    } else {
-      alert("There is an error");
-    }
+  //  } else {
+    //  alert("There is an error");
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   };
 
   
 
   return (
     <div>
-      <h1>React Hooks Forms</h1>
+      <h1>Time Management System</h1>
       
-
+      
       <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Async Submit Validation</h1>
-      <label htmlFor="username">User Name</label>
-      <input name="username" placeholder="Bill" ref={register} />
+      
+      <h1>Employee Login</h1>
+      
+      <label htmlFor="pno">PNO</label>
+      <input name="pno" placeholder="Enter Pno" ref={register} />
 
-      <label htmlFor="lastName">Last Name</label>
-      <input name="lastName" placeholder="Luo" ref={register} />
+      <label htmlFor="password">Password</label>
+      <input name="password" placeholder="Enter your Password" ref={register} />
 
-      <label htmlFor="email">Email</label>
-      <input
-        name="email"
-        placeholder="bluebill1049@hotmail.com"
-        type="text"
-        ref={register}
       />
 
       <div style={{ color: "red" }}>
