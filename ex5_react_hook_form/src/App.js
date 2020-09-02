53ec279f23a9f69e65f9f536d5afd0915fa16dc2
Alay Haider <alay.ffp@gmail.com>
2020-09-02 11:25:59 +0500

    Update App.js

--- ex5_react_hook_form/src/App.js
+++ ex5_react_hook_form/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { useForm } from "react-hook-form";
 
 
@@ -25,24 +25,20 @@ function App() {
 
   return (
     <div>
-      <h1>React Hooks Forms</h1>
+      <h1>TMS Login</h1>
       
 
       <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Async Submit Validation</h1>
-      <label htmlFor="username">User Name</label>
+      
+      <h1>Please enter your PNO and the password for login TMS Website</h1>
+      
+      <label htmlFor="username">Pno</label>
       <input name="username" placeholder="Bill" ref={register} />
 
-      <label htmlFor="lastName">Last Name</label>
+      <label htmlFor="lastName">Password</label>
       <input name="lastName" placeholder="Luo" ref={register} />
 
-      <label htmlFor="email">Email</label>
-      <input
-        name="email"
-        placeholder="bluebill1049@hotmail.com"
-        type="text"
-        ref={register}
-      />
+      
 
       <div style={{ color: "red" }}>
         {Object.keys(errors).length > 0 &&
